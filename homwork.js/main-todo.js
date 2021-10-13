06f3e86e69decaf6b4fdebbf43863b0c732893f1
Osama-alaranji <osamaalaranji@gmail.com>
2021-10-13 17:04:14 +0200

    test task valye by console log

---
+++ homwork.js/main-todo.js
@@ -0,0 +1,20 @@
+//     document.addEventListener("DOMContentLoaded", function(){
+// document.querySelector("form").onsubmit = function(){
+//    let taskname = document.querySelector("#task").value;
+   
+//    document.querySelector("ul").innerHTML = "<li>"  + taskname +   " </li>" ;
+//    return false ;
+// }
+
+//         return false ;
+//     }   )
+
+document.addEventListener("DOMContentLoaded" , function(){
+let task = document.querySelector("task").value ;
+console.log("task")
+
+
+
+
+})
+
