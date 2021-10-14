706319852af5f27274c6682cb28835c63fae42f4
Osama-alaranji <osamaalaranji@gmail.com>
2021-10-14 19:44:26 +0200

    calling  name input . value

--- bmi/main.js
+++ bmi/main.js
@@ -1,5 +1,6 @@
 let weight  = document.querySelector("#weight").value ;
 let height = document.querySelector("#height").value ;
 let subbtn = document.querySelector("#sub") ;
+let name = document.querySelector("#name").value  ;
 
 
