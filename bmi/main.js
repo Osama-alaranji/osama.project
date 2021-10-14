491d068b9c95920ea5c67d9245c98d2457f69ace
Osama-alaranji <osamaalaranji@gmail.com>
2021-10-14 19:26:13 +0200

    get values from inputs

--- bmi/main.js
+++ bmi/main.js
@@ -1,3 +1,5 @@
-let weight  = document.querySelector("#weight");
-let height = document.querySelector("#height");
+let weight  = document.querySelector("#weight").value ;
+let height = document.querySelector("#height").value ;
 let subbtn = document.querySelector("#sub") ;
+
+
