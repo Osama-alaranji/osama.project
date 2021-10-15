7de96c4a05234b29c14b3040b16aa4708b4ef877
Osama-alaranji <osamaalaranji@gmail.com>
2021-10-15 15:20:44 +0200

    adding br and success test

--- bmi/main.js
+++ bmi/main.js
@@ -16,8 +16,9 @@ let form = document.querySelector("form");
 // 2-get weight and height and put them in profile 
 form.onsubmit = function(){
     boldname.innerHTML = "welcome" + " " + name.value ;
-    pcontent.innerHTML = "<b>weight :</b>" + weight.value +"kg" + "<br> <b>height :</b> " + height.value + "cm"
+    pcontent.innerHTML = "<b>weight :</b>" + weight.value +"kg" + "<br><br> <b>height :</b> " + height.value + "cm"
     return false;
+
 
 }
 
