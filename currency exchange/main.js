62f1e0ce788a8b142e6c15856806888db6f4cd03
Osama-alaranji <osamaalaranji@gmail.com>
2021-10-17 15:37:27 +0200

    calling select and add fetch

--- currency exchange/main.js
+++ currency exchange/main.js
@@ -1,13 +1,42 @@
 let exchange = document.querySelector("h1");
 let one = document.querySelector("#one");
 let two = document.querySelector("#two");
-
+let seclect = document.querySelector("select");
 
 
 
 
 fetch("https://api.exchangerate.host/latest").then((getandconvert) => 
  getandconvert.json()).then((final) => {
-     exchange.innerHTML = final.rates.AED ;
- })
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+} // end arrow function
+ ); //end then
+ 
 
