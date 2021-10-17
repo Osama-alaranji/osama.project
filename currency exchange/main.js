5ac0a86018b9853fbfd977e1f9a567e7ebc58db5
Osama-alaranji <osamaalaranji@gmail.com>
2021-10-17 15:41:03 +0200

    calling names from api

--- currency exchange/main.js
+++ currency exchange/main.js
@@ -9,7 +9,8 @@ let seclect = document.querySelector("select");
 fetch("https://api.exchangerate.host/latest").then((getandconvert) => 
  getandconvert.json()).then((final) => {
  
-
+    let names = Object.keys(final.rates);
+    const namearray = {names};
  
  
  
